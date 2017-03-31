74b4b4e70c427c0547689a34130c215e5669f8a5
Hamza <m.hamza20@icloud.com>
2017-03-31 01:37:34 +0000

    initial push

--- linked_list/implementation.py
+++ linked_list/implementation.py
@@ -1,5 +1,5 @@
-from .interface import AbstractLinkedList
-
+from interface import AbstractLinkedList
+from node import Node
 
 class LinkedList(AbstractLinkedList):
     """
@@ -7,16 +7,45 @@ class LinkedList(AbstractLinkedList):
     """
 
     def __init__(self, elements=None):
-        pass
+        if elements is None:
+            self.start = None
+            self.end = None
+        else:
+            self.start = None
+            self.end = None
+            for elem in elements:
+                self.append(elem)
+                
 
     def __str__(self):
-        pass
+        if self.start == None and self.end == None
+            return '[]'
+        else:
+            aux=self.start
+            output = '['
+            while aux is not None:
+                if aux.end is None:
+                    output += str(self.start) + ']'
+                else:
+                    output += str(self.start)
+                aux = aux.next
+                if aux.
+                output += 
 
     def __len__(self):
-        pass
+        if self.start is None:
+            return 0
+        count = 1
+        aux=self.start
+        while aux is not None:
+            aux = aux.next
+            count += 1
 
     def __iter__(self):
-        pass
+        current_node = self.start
+        while current_node != None:
+            yield current_node
+            current_node = current_node.next
 
     def __getitem__(self, index):
         pass
@@ -31,8 +60,14 @@ class LinkedList(AbstractLinkedList):
         pass
 
     def append(self, elem):
-        pass
-
+        newNode = Node(elem)
+        if self.start is None:
+            self.start=newNode
+            self.end=newNode
+        else:
+             aux=self.end
+             aux.next=newNode
+             self.end=aux.next
     def count(self):
         pass
 
--- linked_list/node.py
+++ linked_list/node.py
@@ -2,15 +2,30 @@ class Node(object):
     """
     Node class representing each of the linked nodes in the list.
     """
+    
 
     def __init__(self, elem, next=None):
-        pass
+        self.elem = elem
+        self.next= next
 
     def __str__(self):
-        pass
-
-    def __eq__(self, other):
-        pass
-
+        if self.next:
+            return "Node({}) > Node({})".format(str(self.elem), str(self.next.elem))
+        else:
+            return "Node({}) > /".format(str(self.elem))
+        
+    def __eq__(self, obj):
+        if isinstance(obj,Node):
+            return self.elem == obj.elem and self.next == obj.next
+        else: 
+            return False
+            
+    def __ne__(self, obj):
+        if isinstance(obj,Node):
+            return self.elem != obj.elem or self.next != obj.next
+        else: 
+            return False
+            
     def __repr__(self):
         pass
+        # return 'Node({})'.format(self.elem)
